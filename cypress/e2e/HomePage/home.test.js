cdf71aa5892ad8d6cb41b241ac1bcc1665ef0961
Ammaridrees210 <115483287+Ammaridrees210@users.noreply.github.com>
2024-02-21 21:34:02 +0500

    Project structure

---
+++ cypress/e2e/HomePage/home.test.js
@@ -0,0 +1,7 @@
+import Home from "../../support/pages/home/homePO";
+
+describe("Schedule and dispatch a job", () => {
+  it("should Schedule and dispatch a job", () => {
+    cy.visit("/");
+  });
+});
