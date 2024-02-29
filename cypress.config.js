1ead0942709d934c4d6bba28a912e496ac9233e4
Ammaridrees210 <115483287+Ammaridrees210@users.noreply.github.com>
2024-02-29 16:28:41 +0500

    Schedule and dispatch a job in OT

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl:'https://onetrack.industrack.com/login',
-    specPattern: 'cypress/e2e/**/*.test.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/e2e/**/*.test.{js,jsx,ts,tsx}',
+    chromeWebSecurity : false
   },
+  
 });
